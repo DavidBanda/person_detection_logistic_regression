fde9470c07990c028c477240fe796c9eca838b30
David Banda <davidbandag96@gmail.com>
2020-12-08 10:33:22 -0700

    update name of the hdf5 file

--- ai_backend/utils/lr_utils.py
+++ ai_backend/utils/lr_utils.py
@@ -4,7 +4,7 @@ import h5py
 
 def load_dataset():
     dataset = h5py.File('/Users/davidbanda/Coding/person_detection_logistic_regression/ai_backend/static/dataset'
-                        '/train_personvnonperson.hdf5', "r")
+                        '/train_object.hdf5', "r")
     # your train set features
     train_set_x_orig = np.array(dataset["train_set_x"][:])
     # your train set labels
