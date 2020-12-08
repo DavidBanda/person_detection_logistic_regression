50447c0382ed72469fea2845a529b20a7f05fccb
David Banda <davidbandag96@gmail.com>
2020-12-08 08:12:45 -0700

    initalize the object with the class video camera with a path

--- ai_backend/video_stream/video_camera.py
+++ ai_backend/video_stream/video_camera.py
@@ -4,24 +4,23 @@ import numpy as np
 
 class VideoCamera:
     def __init__(self, path):
-        self.video = cv2.VideoCapture('/Users/davidbanda/Coding/person_detection_logistic_regression/ai_backend'
-                                      '/static/video/person.mp4')
+        self.video = cv2.VideoCapture(path)
         # self.video = cv2.VideoCapture(0)
 
     def __del__(self):
         self.video.release()
 
-    # def get_frame(self):
-    #     ret, image = self.video.read()
-    #     ret, jpeg = cv2.imencode('.jpg', image)
-    #     return jpeg.tobytes()
-
     def get_frame(self):
+        # Obtenemos el frame
         ret, image = self.video.read()
-        # ret, image = cv2.imencode('.jpg', image)
-        frame = cv2.resize(image, (128, 128), interpolation=cv2.INTER_CUBIC)
+
+        # Redimenzionamos el frame a un frame de 128 * 128
+        frame = cv2.resize(image, (128, 128), interpolation=cv2.INTER_AREA)
+        # transformamos la imagen a RGB
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Creamos un arreglo del tipo numpy-array
         frame = np.array(frame[None])
+        # Transformamos el frame de tipo (128, 128, 3) a numpy-array de (128*128*3, 1)
         frame = frame.reshape(frame.shape[0], -1).T
-        frame = frame / 255.
+
         return image, frame
